e6aae889ff5336c9a15b7d0d80dd0850e88269fa
stong <stong@hillinsight.com>
2024-01-17 18:20:41 +0800

    first6

--- server/middleware/checkLogin.js
+++ server/middleware/checkLogin.js
@@ -3,7 +3,7 @@ const {GetUser} = require('../model/UserModel');
 
 
 module.exports = async (ctx,next) => {
-    let openId = ctx.heder['x-wx-openid'];
+    let openId = ctx.header['x-wx-openid'];
     // 白名单；
     let api = ctx.request.url.split('/')[3];
     let apiArr = ['register',];
