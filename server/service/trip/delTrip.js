64ec525ae1f7ca0d935b6a018e2d2bad51d4b7a4
stong <stong@hillinsight.com>
2024-01-24 17:09:31 +0800

    end11

--- server/service/trip/delTrip.js
+++ server/service/trip/delTrip.js
@@ -3,6 +3,11 @@ const {delTrip} = require('../../model/TripModel');
 const paramsCheck = require('../../../utils/paramsCheck');
 const methodBody = require('../../../utils/methodBody');
 
+// 删除行程接口：/api/trip/delTrip
+// 请求方式：post
+// 参数：id-行程的id
+
+
 module.exports = async (ctx, next) => {
     let params = methodBody(ctx);
     
--- server/service/trip/updateTrips.js
+++ server/service/trip/updateTrips.js
@@ -24,7 +24,7 @@ module.exports = async (ctx, next) => {
             inArea:'required|string', // 入境点
             outArea:'required|string', // 出境点
             guideType:'required|string', // 司兼导or司导分, 1是司兼导2是司导分
-            remark:'required|string', // 备注
+            remark:'string', // 备注
     
             guideOpenId:'required|string', // 导游的openId
             color:'required|string', // 创建时候的颜色
