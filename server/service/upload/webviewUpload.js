05c7285db762a669b3232965fc0bb821438a0716
stong <stong@hillinsight.com>
2024-03-12 17:06:26 +0800

    添加webview2

--- server/service/upload/webviewUpload.js
+++ server/service/upload/webviewUpload.js
@@ -8,7 +8,7 @@ const fetch = require('node-fetch');
 module.exports = async (ctx, next) => {
 
     let url = 'https://api.weixin.qq.com/tcb/uploadfile';
-    let body = {env:'prod-5gaf0xgg89478f9e', path:'/'}
+    let body = {env:'prod-5gaf0xgg89478f9e', path:'cloud://prod-5gaf0xgg89478f9e.7072-prod-5gaf0xgg89478f9e-1323848466/'}
 
     let wxRes = await fetch(url, {
         method:'POST',
